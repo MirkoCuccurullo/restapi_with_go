8911510e3bca015a10bf95b40cab8ed5f64f1060
Mirko Cuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-02-28 16:26:22 +0100

    started trivia API

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main 
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/divrhino/divrhino-trivia/database"
+)
 
 func main() {
+
+	database.ConnectDb()
+
 	app := fiber.New()
+	
+	setupRoutes(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Rares!")
+		return c.SendString("Trivia App")
 	})
 
 	app.Listen(":3000")
---
+++ database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/divrhino/divrhino-trivia/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf("host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Europe/Amsterdam",
+	 os.Getenv("DB_USER"), 
+	 os.Getenv("DB_PASSWORD"), 
+	 os.Getenv("DB_NAME")
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database")
+	db.Logger = Logger.Default.LogMode(logger.Info)
+
+	log.Println("Migrating database")
+	db.AutoMigrate(&models.Fact{})
+
+	DB = Dbinstance{Db: db}
+
+}
